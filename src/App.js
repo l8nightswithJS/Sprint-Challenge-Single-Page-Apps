e6efb1d4594a42dbb4a0712ca7ce7963fd060ac4
eddie jimenez <ejmrtears89@gmail.com>
2019-10-11 20:16:10 -0700

    added code to characterlist and route path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage"
+import { Route } from "react-router-dom"
 
 export default function App() {
+  
   return (
     <main>
       <Header />
+      <WelcomePage />
+      <Route exact path='/' component={CharacterList} />
+      <Route path='/CharacterList' component={CharacterList} />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,48 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([])
 
   useEffect(() => {
+    async function fetch(){
+      try{
+        const charactersData = await Axios.get('https://rickandmortyapi.com/api/character/')
+          console.log(charactersData.data.results);
+          setCharacters(charactersData.data.results);
+        }
+      catch(err){
+          console.log(err)
+        }
+    }
+    fetch()
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+  }, [characters]);
+  console.log(characters)
+  
+  
+    return (
+      <section>
+        {characters.map((character) => (
+          <div className="card">
+           <div className="card-body">
+              <h1 className="card-title">
+              {character.name}</h1>
+              <h3 className="character-status">
+              {character.status}             
+              </h3>
+              <h3 className="character-species">{character.species}</h3>
 
-  return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
+            </div>
+          </div>
+        ))}
+        <h1>name</h1>
+  
+      </section>
+    );
 }
+
+
